e4be683f6451c2411e8d9011104bcb121f9e6c3d
Shlok Desai <shlokpdesai@gmail.com>
2020-03-29 18:58:59 -0700

    Add explore page

--- domain/explore.js
+++ domain/explore.js
@@ -1,7 +1,19 @@
 export const getExploreModel = () => {
-    return {
-        rides: [{ name: "Crazy Cups"}, { name: "Roller Coaster"}],
-        restaurants: [{ name: "Sandwich Shop"}, { name: "Milkshake Space"}],
-        shows: [{ name: "Jugglers Row"}, { name: "Animals with Annie"}]
-    }
-}
+  return {
+    rides: [
+      { name: "Ferris Wheel", image: "/ferriswheel.jpg", description: "Marvel at the exotic views" },
+      { name: "Cliffhanger", image: "/cliffhanger.jpg", description: "Thrills and spills" },
+      { name: "Dodgems", image: "/dodgems.jpg", description: "Dodge and hit your friends" },
+      { name: "Merry-Go-Round", image: "/carousel.jpg", description: "With horses and lions and bears, oh my!" }
+    ],
+    restaurants: [
+      { name: "Sandwich Shop", image: "/sandwich.jpg", description: "Served with freshly baked bread" },
+      { name: "Milkshake Space", image: "/milkshakes.jpg", description: "Enjoy some succulent concoctions" },
+      { name: "Baljit's Burgers", image: "/burger.jpg", description: "Yummy, juicy burgers that everyone will love." }
+    ],
+    shows: [
+      { name: "Jugglers Row", image: "/jugglers.jpg", description: "Meet our fine crew of fantastic jugglers" },
+      { name: "Animals with Ananya", image: "/animals.jpg", description: "Our exotic menagerie puts on a show" }
+    ]
+  };
+};
--- pages/explore.js
+++ pages/explore.js
@@ -1,13 +1,35 @@
 import { getExploreModel } from "../domain/explore";
 import { Fragment, useState } from "react";
 
-const Accordion = ({ title, children }) => {
-  const [ expanded, setExpanded ] = useState(false);
-  const height = expanded ? 330 : 0;
+const Accordion = ({ title, children, containerClasses }) => {
+  const [expanded, setExpanded] = useState(false);
+  const maxHeight = expanded ? 50000 : 0;
   return (
     <div className="flex flex-column pa4 ma2 justify-center items-center w-100">
-      <button className="b pa4 w-90 f1 light-purple bg-transparent b--dashed pointer accordion-button" onClick={() => setExpanded(!expanded)}>{title}</button>
-      <div style={{ height }} className={`overflow-hidden animate-height`}>{children}</div>
+      <button
+        className="b pa4 w-90 f1 light-purple bg-transparent b--dashed pointer accordion-button"
+        onClick={() => setExpanded(!expanded)}
+      >
+        {title}
+      </button>
+      <div
+        style={{ maxHeight }}
+        className={`${containerClasses} overflow-hidden animate-height`}
+      >
+        {children}
+      </div>
+    </div>
+  );
+};
+
+const AttractionCard = ({ name, image, description }) => {
+  return (
+    <div className="pa2 ma2 flex items-center justify-center bg-white w-50 h-50 shadow-4">
+      <img className="h-50 w-50" alt={name} src={image} />
+      <div className="ma2 pa2 flex flex-column tc">
+        <p className="f2">{name}</p>
+        <p>{description}</p>
+      </div>
     </div>
   );
 };
@@ -16,24 +38,37 @@ const ExplorePage = ({ rides, restaurants, shows }) => {
   return (
     <Fragment>
       <h1 className="b white tc f-headline">So Much to Do and See!</h1>
-      <img className="h-50 w-50 db mr-auto ml-auto shadow-4" src="/explore.jpg" />
+      <img
+        className="h-33 w-33 db mr-auto ml-auto shadow-4"
+        src="/explore.jpg"
+      />
       <p className="white pa4 tc f3">
-      Come be immersed in a land full of fun things to do for the whole family. 
-      Take a thriling ride, eat some yummy food and marvel at our amazing shows.
+        Come be immersed in a land full of fun things to do for the whole
+        family. Take a thriling ride, eat some yummy food and marvel at our
+        amazing shows.
       </p>
-      <Accordion title="Rides">
-        {rides.map(ride => (
-          <p className="white">{ride.name}</p>
+      <Accordion
+        containerClasses="flex flex-wrap items-center justify-center mv2"
+        title="Rides"
+      >
+        {rides.map((ride, index) => (
+          <AttractionCard key={index} {...ride} />
         ))}
       </Accordion>
-      <Accordion title="Restaurants">
-        {restaurants.map(restaurant => (
-          <p className="white">{restaurant.name}</p>
+      <Accordion
+        containerClasses="flex flex-wrap items-center justify-center mv2"
+        title="Restaurants"
+      >
+        {restaurants.map((restaurant, index) => (
+          <AttractionCard key={index} {...restaurant} />
         ))}
       </Accordion>
-      <Accordion title="Shows">
-        {shows.map(show => (
-          <p className="white">{show.name}</p>
+      <Accordion
+        containerClasses="flex flex-wrap items-center justify-center mv2"
+        title="Shows"
+      >
+        {shows.map((show, index) => (
+          <AttractionCard key={index} {...show} />
         ))}
       </Accordion>
     </Fragment>
